58b436aa94fe3677673e2cf2fa290e23707ac153
duytung0608 <binlaid123@gmail.com>
2022-07-27 18:46:31 +0700

    update ss3 file

---
+++ src/ss3_arrayandmethod_in_java/CountOfACharacterInAString.java
@@ -0,0 +1,15 @@
+package ss3_arrayandmethod_in_java;
+
+public class CountOfACharacterInAString {
+    public static void main(String[] args) {
+        String string = "CodegymC";
+        char characters = 'C';
+        int count = 0;
+        for (int i=0;i<string.length();i++) {
+            if (string.charAt(i) == characters) {
+                count++;
+            }
+        }
+        System.out.println("Count is: " + count);
+    }
+}
